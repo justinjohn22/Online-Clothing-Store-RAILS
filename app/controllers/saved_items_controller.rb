0d8767e029a3f6cb1256254da6c1baae19a4db94
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-05-10 20:55:58 +1000

    adding controllers and functionality for saved list and saved items

--- app/controllers/saved_items_controller.rb
+++ app/controllers/saved_items_controller.rb
@@ -1,2 +1,27 @@
 class SavedItemsController < ApplicationController
+    def create
+        # Find associated product and current saved_list
+        chosen_product = Product.find(params[:product_id])
+        current_saved_list = @current_saved_list
+      
+        # If saved_list already has this product then find the relevant saved_item for this product
+        if current_saved_list.products.include?(chosen_product)
+          # Find the saved_item with the chosen_product
+          @saved_item = current_saved_list.saved_items.find_by(:product_id => chosen_product)
+          # Iterate the saved_item's quantity by one
+          @saved_item.destroy
+        else
+          @saved_item = SavedItem.new
+          @saved_item.saved_list = current_saved_list
+          @saved_item.product = chosen_product
+        end
+        # s
+        # Save and redirect to cart show path
+        @saved_item.save
+        redirect_to saved_list_path(current_saved_list)
+    end
+    private 
+        def line_item_params
+            params.require(:line_item).permit(:quantity,:product_id, :cart_id)
+        end
 end
