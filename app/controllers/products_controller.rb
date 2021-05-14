dda2fad414533d872e314bf623bb6374d6d761f2
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-05-14 12:41:48 +1000

    Created a Product Details page for each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,8 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def details(product)
-    @product_description = product.description
-    @product_url1 = product.image_url
-    @product_url2 = product.image_url2
-    @product_url3 = product.image_url3
+  def details
+    @product = Product.find(params[:id])
   end
 
   def women
