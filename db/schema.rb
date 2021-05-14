2e3b9020d73fa70da3b27dfccdb12c300a0e0627
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-05-14 23:33:50 +1000

    Finished Product Details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210512094322) do
+ActiveRecord::Schema.define(version: 20210514125837) do
+
+  create_table "bags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
