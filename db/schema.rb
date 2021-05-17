1689d55f199e4464c5bc0af32438feda30602e1f
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-05-17 17:25:25 +1000

    merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,11 @@ ActiveRecord::Schema.define(version: 20210516224906) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
+    t.text "description"
     t.string "collection_type"
     t.string "image_url"
+    t.string "image_url2"
+    t.string "image_url3"
     t.decimal "popularity"
     t.date "date_arrived"
     t.datetime "created_at", null: false
