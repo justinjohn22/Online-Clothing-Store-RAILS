07dcbd573a2d4c9e95e1cbb168e0268656a6af72
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-05-18 11:17:20 +1000

    styling for women page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   config.assets.precompile += ['home.scss']
   config.assets.precompile += ['home.js']
 
+  config.assets.precompile += ['women.scss']
+  config.assets.precompile += ['women.js']
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
