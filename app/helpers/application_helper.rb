9f2ed471d6192dd390f23f0c6d84b7cc3c84f39e
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-05-21 17:22:45 +1000

    Implementing Order sessions help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
   def current_order
     if !session[:order_id].nil?
-      Order.find(session[:order_id])
+      @find_order = Order.find(session[:order_id]) rescue nil
+      if !@find_order
+        session[:order_id] = nil
+        current_order
+      elsif @find_order.order_status_id != 1
+        session[:order_id] = nil
+        current_order
+      else
+        Order.find(session[:order_id])
+      end
     else
       Order.new
     end
