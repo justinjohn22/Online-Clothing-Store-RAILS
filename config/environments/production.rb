a984cb7e277545856133450722df6d4401c24fa7
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-05-23 23:41:11 +1000

    Testing percompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,16 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += ['home.scss']
+  config.assets.precompile += ['home.js']
+
+  config.assets.precompile += ['collection_pages.scss']
+  config.assets.precompile += ['collection_pages.js']
+  config.assets.precompile += ['customer_logged.scss']
+  config.assets.precompile += ['log_on.scss']
+  
+  config.assets.precompile += ['products.scss']
+  config.assets.precompile += ['products.js']
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
