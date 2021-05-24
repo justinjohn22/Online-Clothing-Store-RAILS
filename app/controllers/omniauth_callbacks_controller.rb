fe5036764a9925599afcd1a51879eda9984c4508
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-05-24 11:20:42 +1000

    Adding API keys onto heroku

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class OmniauthCallbacksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :twitter
   def twitter
     render plain: "Success! You Have logged in via twitter"
   end
